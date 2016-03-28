053314e63dfd09f1a84ed7acf1338f519757415e
Nick B <nick@declension.net>
2016-03-28 23:36:38 +0100

    Show commit message for each commit
     * Extract to method, and spend _far_ too long wrangling types
     * Remove redundant config
     * Tidy deps

--- app/Main.hs
+++ app/Main.hs
@@ -5,29 +5,35 @@ module Main (main) where
 import           Control.Logging
 import           Control.Applicative
 import           Data.Maybe
+import           Control.Monad
+import           Control.Monad.Trans.State
 import           Data.Monoid
 import           Data.Tagged
 import qualified Data.Text as T
 import           Git
-import           Git.Libgit2 (MonadLg, LgRepo, lgFactory)
+import           Git.Libgit2 (MonadLg, lgFactory)
 
 main = withStdoutLogging $ do
     log' "Starting"
-    let repoOpts = RepositoryOptions { repoPath = "."
-                                     , repoWorkingDir = Nothing
-                                     , repoIsBare = False
-                                     , repoAutoCreate = False
-                                     }
     let dir = "."
     withRepository lgFactory dir $ do
-        ref <- lookupReference "HEAD"
         ref' <- resolveReference "HEAD"
         case ref' of
             Just r -> do
-                log' $ "Got refs: " <> T.pack (show r)
+                log' $ T.pack $ "Got refs: " ++ show r
                 commit   <- lookupCommit (Tagged r)
+                log' $ T.pack $ show $ commitLog commit
                 commits <- listCommits Nothing (commitOid commit)
                 log' "Here are the commits: "
-                mapM_ (log' . T.pack . show) commits
+                msgs <- mapM getMessage commits
+                mapM_ log' msgs
             _ -> log' "No branch is checked out"
     log' "Finished"
+
+
+getMessage :: MonadGit r m => CommitOid r -> m T.Text
+getMessage commitOid = do
+    commit <- lookupCommit commitOid
+    let text = show (commitLog commit)
+    let str = "Commit " ++ show (untag commitOid) ++ ": " ++ text
+    return $ T.pack str
